9449006536a3c2e1510b3de128cf8e750606795d
EmSley77 <emanuelsleyman@gmail.com>
2025-03-22 12:15:37 +0100

    fixing log out

--- src/Components/Shared/Header.jsx
+++ src/Components/Shared/Header.jsx
@@ -1,4 +1,4 @@
-import { SupabaseClient } from '@supabase/supabase-js'
+import { supabaseClient } from '../../Helper/supabase-helper.js'
 import React from 'react'
 import { NavLink, useNavigate } from 'react-router'
 import '../../Styles/header.css'
@@ -8,7 +8,7 @@ export default function Header() {
 
   const navigate = useNavigate()
   const handleLogOut = async () => {
-    let { error } = await SupabaseClient.auth.signOut()
+    let { error } = await supabaseClient.auth.signOut()
     if (error) throw error
     navigate("/")
   }
@@ -20,7 +20,7 @@ export default function Header() {
       justifyContent: "space-around",
       background: "linear-gradient(to right, #390057, #650399, #b803b8, #ff00ff)",
     }}>
-      <NavLink className='header-link' to={"/"} onClick={handleLogOut}>Logout</NavLink>
+      <NavLink className='header-link' onClick={handleLogOut}>Logout</NavLink>
       <NavLink className='header-link' to={"/home"}>Home</NavLink>
       <NavLink className='header-link' to={`/account`}>Account</NavLink>
       {/*    <NavLink className='header-link'>Liked</NavLink>
